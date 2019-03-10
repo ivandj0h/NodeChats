dce3bd95f222ec531bf34141533f1c163e0abb62
IVANDI <41560792+Ivandidjoh@users.noreply.github.com>
2019-03-10 17:36:30 +0700

    Create & Configure the entry point file that is Index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+// Here is How we Configure the Express.Js
+var express = require('express');
+
+var app = express();
+
+var server = require('http').createServer(app);
+
+var io = require('socket.io').listen(server);
+
+var path = require('path');
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
+
+server.listen(process.env.PORT || 3000, () => console.log('Server are now Listening...'));
+
+
+// Now We Create the Routes
+app.get('/', (req, res) => {
+    res.render('Chat');
+});
