8318ca9902ddf1cbf9261e628ca3306ab2dd8fd0
IVANDI <41560792+Ivandidjoh@users.noreply.github.com>
2019-03-10 18:52:57 +0700

    modified chat

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
-// Here is How we Configure the Express.Js
+
+// Make a Configuration in Express.
 var express = require('express');
-
 var app = express();
-
 var server = require('http').createServer(app);
-
 var io = require('socket.io').listen(server);
-
 var path = require('path');
-
-
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
-
-server.listen(process.env.PORT || 3000, () => console.log('Server are now Listening...'));
-
-
-// Now We Create the Routes
-app.get('/', (req, res) => {
-    res.render('Chat');
+server.listen(process.env.PORT || 3000, function() {
+  console.log('Server listening');
 });
 
 
-// Web Socket Configurations
-io.sockets.on('connection', (socket) => {
-    socket.on('chat-message', (message) => {
-        io.sockets.emit('chat-message', message);
+// Create the Routes
+app.get("/", function(req, res) {
+    res.render("chat");
+});
+
+// We Configure web sockets.
+io.sockets.on("connection", function(socket) {
+
+    socket.on("chat-message", function(message) {
+        io.sockets.emit("chat-message", message);
     });
+
 });
